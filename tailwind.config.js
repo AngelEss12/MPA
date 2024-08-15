95ffe9997f732961a962624c441752b5a8ff12d8
Miguel Prudencio <mprudenciom01@gmail.com>
2024-08-15 09:40:20 -0600

    Seccion de virtudes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        titulos: ["Dancing Script", "cursive"],
+        cuerpos:["Raleway", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
