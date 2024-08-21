2854128eed5a1a0967b83ff7e788c4b8423354fc
Miguel Angel Prudencio Morales <134902462+AngelEss12@users.noreply.github.com>
2024-08-21 00:04:07 -0600

    Scrip animaciones

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const updateBubblePositions = () => {
+        const burbujas = document.querySelectorAll("#burbujas > div");
+        const container = document.getElementById('burbujas');
+        const containerWidth = container.clientWidth;
+        const containerHeight = container.clientHeight;
+
+        burbujas.forEach(burbuja => {
+            const burbujaSize = burbuja.offsetWidth; // Tamaño de la burbuja
+
+            // Máximas posiciones permitidas
+            const maxX = containerWidth - burbujaSize;
+            const maxY = containerHeight - burbujaSize;
+
+            // Posiciones aleatorias para bottom, left, y right
+            const randomBottom = Math.floor(Math.random() * maxY);
+            const randomLeftOrRight = Math.random() > 0.5 ? 'left' : 'right';
+            const randomX = Math.floor(Math.random() * maxX);
+
+            // Asignar las nuevas posiciones
+            burbuja.style.position = "absolute";
+            burbuja.style.bottom = `-${randomBottom}px`;
+            burbuja.style[randomLeftOrRight] = `${randomX}px`;
+
+            // Remover la propiedad contraria para evitar conflictos
+            if (randomLeftOrRight === 'left') {
+                burbuja.style.right = 'auto';
+            } else {
+                burbuja.style.left = 'auto';
+            }
+        });
+    };
+
+    // Ejecutar la función inmediatamente al cargar la página
+    updateBubblePositions();
+
+    // Repetir la ejecución de la función cada 8 segundos
+    setInterval(updateBubblePositions, 8000);
+});
