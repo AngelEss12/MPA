c347675b03fcb4bbe6fc3e2bf93f60c219b042d6
Miguel Angel Prudencio Morales <134902462+AngelEss12@users.noreply.github.com>
2024-09-13 18:18:23 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,14 @@ module.exports = {
         titulos: ["Dancing Script", "cursive"],
         cuerpos:["Raleway", "sans-serif"],
       },
-
+      backgroundImage: {
+        'gradiente-inicio': 'linear-gradient(to left top, #efceff, #ffcde5, #ffd7c5, #ffeab3, #ebffbf)',
+        'gradiente-v1': 'linear-gradient(to right bottom, #cef3ff, #cef8fa, #d3fbf3, #ddfeeb, #ebffe4)',
+        'gradiente-footer': 'linear-gradient(to right bottom, #ffd0c5, #ffd7be, #ffdfba, #f7e8b9, #ebf2be, #d6f7c8, #c1fad7, #affce9, #a2f6fc, #a8ecff, #bde1ff, #d4d5ff)',
+      },
       keyframes: {
         appear: {
-          '0%': { opacity: '0', transform: 'scale(0.85)' }, // Escala inicial al 25%
+          '0%': { opacity: '0', transform: 'scale(0.75)' }, // Escala inicial al 25%
           '100%': { opacity: '1', transform: 'scale(1)' },   // Escala final al 100%
         },
 
@@ -42,6 +46,10 @@ module.exports = {
           '0%': { transform: 'translateY(0) scale(1)' },
           '100%': { transform: 'translateY(-1150px) scale(1.4)' },
         },
+        bubbledown: {
+          '0%': { transform: 'translateY(0) scale(1)' },
+          '100%': { transform: 'translateY(1150px) scale(1.4)' },
+        },
 
         positionSwap: {
           '0%': { transform: 'translate(0, 0)' },
@@ -63,6 +71,14 @@ module.exports = {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+        slideIn: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        slideOut: {
+          '0%': { transform: 'translateX(0)', opacity: '1' },
+          '100%': { transform: 'translateX(-100%)', opacity: '0' },
+        },
       },
       animation: {
         appear: 'appear 0.1s ease-out both',  // Define la animación con la duración y timing-function que desees
@@ -78,7 +94,18 @@ module.exports = {
         'bubbleup-delayed-6': 'bubbleup 8s infinite 5s',
         'bubbleup-delayed-7': 'bubbleup 8s infinite 7s',
         'position-swap': 'positionSwap 8s infinite',
+        bubbledown: 'bubbledown 8s infinite',
+        'bubbledown-delayed-1': 'bubbledown 8s infinite 2s',
+        'bubbledown-delayed-2': 'bubbledown 8s infinite 4s',
+        'bubbledown-delayed-3': 'bubbledown 8s infinite 6s',
+        'bubbledown-delayed-4': 'bubbledown 8s infinite 1s',
+        'bubbledown-delayed-5': 'bubbledown 8s infinite 3s',
+        'bubbledown-delayed-6': 'bubbledown 8s infinite 5s',
+        'bubbledown-delayed-7': 'bubbledown 8s infinite 7s',
+        'position-swap': 'positionSwap 8s infinite',
         spinright: 'spinright 16s linear infinite',
+        slidein: 'slideIn 1s forwards',
+        slideout: 'slideOut 1s forwards',
       },
       
     },
