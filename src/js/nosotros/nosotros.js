482c5cada2c20a7bb719ebb44ac4d4279f327832
Miguel Angel Prudencio Morales <134902462+AngelEss12@users.noreply.github.com>
2025-01-11 14:04:47 -0600

    Update nosotros.js

--- src/js/nosotros/nosotros.js
+++ src/js/nosotros/nosotros.js
@@ -1,14 +1,16 @@
 import { nosotros } from './variables.js';
 import { updateBubblePositions } from '../animaciones/animaciones.js';
-import { moveLeft, moveRight } from './navegacionIdentidad.js'
+import { moveLeft, moveRight, nextVirtud, backVirtud } from './navegacionIdentidad.js'
 import { initTouchListeners } from './touchListeners.js';
 import { logoActions, himnoActions, creadorActions } from './identidad.js';
 import { animacionHistory } from './historia.js'
+import { actualizarContador } from './momentos.js'
 
 //  Cuando el documento carga
 document.addEventListener("DOMContentLoaded", () => {
-    // Inicializa los listeners touch
-    initTouchListeners();
+    // Inicializa los listeners touch de las secciones de identidad y momentos
+    initTouchListeners(nosotros.identidad.contenido, moveLeft, moveRight);
+    initTouchListeners(nosotros.momentos.slider, nextVirtud, backVirtud);
 
     // Ejecutar la función inmediatamente al cargar la página
     logoActions.init();
@@ -72,7 +74,6 @@ const poitns = document.querySelectorAll('.point')
 const prevBtn = document.querySelector('prevBtn');
 const nextBtn = document.querySelector('nextBtn');
 
-// Variables para rastrear el estado actual del slider
 let currentIndex = 0;
 let totalCards = document.querySelectorAll('.card').length;
 
@@ -82,83 +83,82 @@ const mediaQuery = window.matchMedia('(min-width: 640px)');
 // Inicializamos el contador
 let contador = 0;
 
-// Función para actualizar el valor del contador según el tamaño de la pantalla
-function actualizarContador(e) {
-    if (e.matches) {
-        contador = 2; // Si la pantalla es 768px o más, el contador es 2
-    } else {
-        contador = 1; // Si la pantalla es menor a 768px, el contador es 1
-    }
-}
+// function actualizarContador(e) {
+//     if (e.matches) {
+//         contador = 2; // Si la pantalla es 768px o más, el contador es 2
+//     } else {
+//         contador = 1; // Si la pantalla es menor a 768px, el contador es 1
+//     }
+// }
 
 // Escuchamos los cambios en el tamaño de la pantalla
-mediaQuery.addListener(actualizarContador);
+// mediaQuery.addListener(actualizarContador);
 
-// Llamamos la función inmediatamente para comprobar el tamaño actual de la pantalla
-actualizarContador(mediaQuery);
+// // Llamamos la función inmediatamente para comprobar el tamaño actual de la pantalla
+// actualizarContador(mediaQuery);
 
-// Listener para eventos touch
-slider.addEventListener("touchstart", (event) => {
-    // Captura la posición inicial del touch
-    touchStartX = event.changedTouches[0].screenX;
-    touchStartY = event.changedTouches[0].screenY;
-});
+// // Listener para eventos touch
+// slider.addEventListener("touchstart", (event) => {
+//     // Captura la posición inicial del touch
+//     touchStartX = event.changedTouches[0].screenX;
+//     touchStartY = event.changedTouches[0].screenY;
+// });
 
-slider.addEventListener("touchend", (event) => {
-    // Captura la posición final del touch
-    touchEndX = event.changedTouches[0].screenX;
-    touchEndY = event.changedTouches[0].screenY;
+// slider.addEventListener("touchend", (event) => {
+//     // Captura la posición final del touch
+//     touchEndX = event.changedTouches[0].screenX;
+//     touchEndY = event.changedTouches[0].screenY;
 
-    // Llama a la función para manejar el movimiento
-    touchMoveMoments();
-});
+//     // Llama a la función para manejar el movimiento
+//     touchMoveMoments();
+// });
 
-const touchMoveMoments = () => {
-    // Calcula las diferencias entre las posiciones de inicio y fin
-    const deltaX = touchEndX - touchStartX;
-    const deltaY = touchEndY - touchStartY;
+// const touchMoveMoments = () => {
+//     // Calcula las diferencias entre las posiciones de inicio y fin
+//     const deltaX = touchEndX - touchStartX;
+//     const deltaY = touchEndY - touchStartY;
 
-    // Verifica si el deslizamiento es más horizontal que vertical y si supera el umbral
-    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > umbralTouch) {
-        // Si el deslizamiento fue hacia la derecha
-        if (deltaX < 0) {
-            nextVirtud();
-        }
-        // Si el deslizamiento fue hacia la izquierda
-        else if (deltaX > 0) {
-            backVirtud();
-        }
-    }
-};
+//     // Verifica si el deslizamiento es más horizontal que vertical y si supera el umbral
+//     if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > umbralTouch) {
+//         // Si el deslizamiento fue hacia la derecha
+//         if (deltaX < 0) {
+//             nextVirtud();
+//         }
+//         // Si el deslizamiento fue hacia la izquierda
+//         else if (deltaX > 0) {
+//             backVirtud();
+//         }
+//     }
+// };
 
-const nextVirtud = () => {
-    poitns[currentIndex].classList.remove("bg-slate-500");
-    if (currentIndex < totalCards - contador) {
-        currentIndex++;
-        updateSliderPosition();
-    } else {
-        currentIndex = 0;
-        updateSliderPosition();
-    }
-}
+// const nextVirtud = () => {
+//     poitns[currentIndex].classList.remove("bg-slate-500");
+//     if (currentIndex < totalCards - contador) {
+//         currentIndex++;
+//         updateSliderPosition();
+//     } else {
+//         currentIndex = 0;
+//         updateSliderPosition();
+//     }
+// }
 
-const backVirtud = () => {
-    poitns[currentIndex].classList.remove("bg-slate-500");
-    if (currentIndex > 0) {
-        currentIndex--;
-        updateSliderPosition();
-    } else {
-        currentIndex = totalCards - contador;
-        updateSliderPosition();
-    }
-}
+// const backVirtud = () => {
+//     poitns[currentIndex].classList.remove("bg-slate-500");
+//     if (currentIndex > 0) {
+//         currentIndex--;
+//         updateSliderPosition();
+//     } else {
+//         currentIndex = totalCards - contador;
+//         updateSliderPosition();
+//     }
+// }
 
-// Función para actualizar la posición del slider
-const updateSliderPosition = () => {
-    const slideWidth = document.querySelector('.card').offsetWidth;
-    slider.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
-    poitns[currentIndex].classList.add('bg-slate-500');
-};
+// // Función para actualizar la posición del slider
+// const updateSliderPosition = () => {
+//     const slideWidth = document.querySelector('.card').offsetWidth;
+//     slider.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+//     poitns[currentIndex].classList.add('bg-slate-500');
+// };
 
 const teamSelector = document.querySelectorAll('#equipo > div');
 const teamNinos = document.querySelector('#niños');
