dd24d4aedb691ed4f0047d3bbe94f715a159fcae
Miguel Angel Prudencio Morales <134902462+AngelEss12@users.noreply.github.com>
2025-01-13 16:55:23 -0600

    Agregando últimos módulos

--- src/js/nosotros/nosotros.js
+++ src/js/nosotros/nosotros.js
@@ -1,15 +1,18 @@
 import { nosotros } from './variables.js';
 import { updateBubblePositions } from '../animaciones/animaciones.js';
-import { moveLeft, moveRight, nextVirtud, backVirtud } from './navegacionIdentidad.js'
+import { moveLeft, moveRight, nextVirtud, backVirtud } from './navegacionIdentidad.js';
 import { initTouchListeners } from './touchListeners.js';
 import { logoActions, himnoActions, creadorActions } from './identidad.js';
-import { animacionHistory } from './historia.js'
-import { actualizarContador } from './momentos.js'
+import { animacionHistory } from './historia.js';
+import { actualizarContador } from './momentos.js';
+import { mostarTeam } from './equipo.js';
 
 //  Cuando el documento carga
 document.addEventListener("DOMContentLoaded", () => {
     // Inicializa los listeners touch de las secciones de identidad y momentos
-    initTouchListeners(nosotros.identidad.contenido, moveLeft, moveRight);
+    initTouchListeners(nosotros.identidad.contenido, moveRight, moveLeft);
+
+    // Inicializamos los eventos para mover las cards
     initTouchListeners(nosotros.momentos.slider, nextVirtud, backVirtud);
 
     // Ejecutar la función inmediatamente al cargar la página
@@ -17,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     updateBubblePositions();
 
     // Repetir la ejecución de la función cada 8 segundos
-    setInterval(updateBubblePositions, 10000);
+    setInterval(updateBubblePositions, 12000);
 });
 
 const { arrowLeft, arrowRight } = nosotros.identidad;
@@ -69,103 +72,25 @@ nosotros.historia.displayHistorias.forEach(div => {
 });
 
 // Momentos
-const slider = document.querySelector('.slider');
-const poitns = document.querySelectorAll('.point')
-const prevBtn = document.querySelector('prevBtn');
-const nextBtn = document.querySelector('nextBtn');
-
-let currentIndex = 0;
-let totalCards = document.querySelectorAll('.card').length;
-
-// Creamos una media query para detectar si la pantalla es mayor o igual a 768px
-const mediaQuery = window.matchMedia('(min-width: 640px)');
-
-// Inicializamos el contador
-let contador = 0;
-
-// function actualizarContador(e) {
-//     if (e.matches) {
-//         contador = 2; // Si la pantalla es 768px o más, el contador es 2
-//     } else {
-//         contador = 1; // Si la pantalla es menor a 768px, el contador es 1
-//     }
-// }
-
 // Escuchamos los cambios en el tamaño de la pantalla
-// mediaQuery.addListener(actualizarContador);
+const { mediaQuery } = nosotros.momentos;
+mediaQuery.addListener(actualizarContador);
 
-// // Llamamos la función inmediatamente para comprobar el tamaño actual de la pantalla
-// actualizarContador(mediaQuery);
+// Llamamos la función inmediatamente para comprobar el tamaño actual de la pantalla
+actualizarContador(mediaQuery);
 
-// // Listener para eventos touch
-// slider.addEventListener("touchstart", (event) => {
-//     // Captura la posición inicial del touch
-//     touchStartX = event.changedTouches[0].screenX;
-//     touchStartY = event.changedTouches[0].screenY;
-// });
+const { prevBtn, nextBtn } = nosotros.momentos;
 
-// slider.addEventListener("touchend", (event) => {
-//     // Captura la posición final del touch
-//     touchEndX = event.changedTouches[0].screenX;
-//     touchEndY = event.changedTouches[0].screenY;
+prevBtn.addEventListener("click", () => {
+    backVirtud();
+});
 
-//     // Llama a la función para manejar el movimiento
-//     touchMoveMoments();
-// });
+nextBtn.addEventListener("click", () => {
+    nextVirtud();
+});
 
-// const touchMoveMoments = () => {
-//     // Calcula las diferencias entre las posiciones de inicio y fin
-//     const deltaX = touchEndX - touchStartX;
-//     const deltaY = touchEndY - touchStartY;
-
-//     // Verifica si el deslizamiento es más horizontal que vertical y si supera el umbral
-//     if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > umbralTouch) {
-//         // Si el deslizamiento fue hacia la derecha
-//         if (deltaX < 0) {
-//             nextVirtud();
-//         }
-//         // Si el deslizamiento fue hacia la izquierda
-//         else if (deltaX > 0) {
-//             backVirtud();
-//         }
-//     }
-// };
-
-// const nextVirtud = () => {
-//     poitns[currentIndex].classList.remove("bg-slate-500");
-//     if (currentIndex < totalCards - contador) {
-//         currentIndex++;
-//         updateSliderPosition();
-//     } else {
-//         currentIndex = 0;
-//         updateSliderPosition();
-//     }
-// }
-
-// const backVirtud = () => {
-//     poitns[currentIndex].classList.remove("bg-slate-500");
-//     if (currentIndex > 0) {
-//         currentIndex--;
-//         updateSliderPosition();
-//     } else {
-//         currentIndex = totalCards - contador;
-//         updateSliderPosition();
-//     }
-// }
-
-// // Función para actualizar la posición del slider
-// const updateSliderPosition = () => {
-//     const slideWidth = document.querySelector('.card').offsetWidth;
-//     slider.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
-//     poitns[currentIndex].classList.add('bg-slate-500');
-// };
-
-const teamSelector = document.querySelectorAll('#equipo > div');
-const teamNinos = document.querySelector('#niños');
-const teamAsesor = document.querySelector('#asesor');
-const teamTios = document.querySelector('#tios');
-const teamPapis = document.querySelector('#papis');
-
+// Equipo
+const { teamNinos, teamAsesor, teamTios, teamPapis } = nosotros.equipo;
 teamNinos.addEventListener('click', () => {
     mostarTeam(0);
 });
@@ -181,21 +106,3 @@ teamTios.addEventListener('click', () => {
 teamPapis.addEventListener('click', () => {
     mostarTeam(3);
 });
-
-function mostarTeam(n) {
-    teamSelector.forEach(team => team.classList.add('hidden'));
-    switch (n) {
-        case 0:
-            teamSelector[n].classList.remove('hidden');
-            break;
-        case 1:
-            teamSelector[n].classList.remove('hidden');
-            break;
-        case 2:
-            teamSelector[n].classList.remove('hidden');
-            break;
-        case 3:
-            teamSelector[n].classList.remove('hidden');
-            break;
-    }
-}
