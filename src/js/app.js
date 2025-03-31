feabbf5b98fb39297aedf4c05837b45a81439636
Miguel Prudencio <mprudenciom01@gmail.com>
2025-03-31 16:41:33 -0600

    ECMaScript en el inicio 2

--- src/js/app.js
+++ src/js/app.js
@@ -19,40 +19,6 @@ menuBurger.addEventListener('click', () => {
   body.classList.toggle('overflow-hidden');
 });
 
-// const leyes = () => {
-//   const slides = document.querySelectorAll("#leyes a, #leyOro");
-//   let currentIndex = 0;
-
-//   // Mostrar el primer slide al cargar
-//   slides[currentIndex].classList.remove("hidden");
-//   slides[currentIndex].classList.add("entrada");
-
-//   setInterval(() => {
-//     // Animar el slide actual para desaparecer (fade out)
-//     slides[currentIndex].classList.remove("entrada");
-//     slides[currentIndex].classList.add("salida");
-
-//     // Después de 1 segundo (duración de la animación fade out), ocultar el slide actual
-//     setTimeout(() => {
-//       slides[currentIndex].classList.add("hidden");
-//       slides[currentIndex].classList.remove("salida");
-
-//       // Pasar al siguiente slide
-//       currentIndex = (currentIndex + 1) % slides.length;
-
-//       // Si estamos en el slide 6 (último slide), volver al slide 1
-//       if (currentIndex === slides.length - 1) {
-//         currentIndex = 0; // Volver al slide 1 cuando llegamos al último slide
-//       }
-
-//       // Mostrar el siguiente slide con fade in
-//       slides[currentIndex].classList.remove("hidden");
-//       slides[currentIndex].classList.add("entrada");
-
-//     }, 700); // Tiempo de fade out (1s)
-//   }, 5000); // Cambiar cada 5 segundos
-// }
-
 const slider = document.querySelector('.slider');
 const cards = document.querySelectorAll('.card');
 const poitns = document.querySelectorAll('.point')
